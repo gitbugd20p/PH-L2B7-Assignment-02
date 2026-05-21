e2c8a4b3c686ef4d3fce9b75edaea29fc4f3a718
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2026-05-21 17:57:04 +0600

    added db-schema for users and issues

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,5 +6,32 @@ export const pool = new Pool({
 });
 
 export const initDB = async () => {
-    console.log("DB Connected!.");
+    try {
+        await pool.query(`CREATE TABLE IF NOT EXISTS users(
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(50) NOT NULL,
+			email VARCHAR(255) NOT NULL UNIQUE,
+			password VARCHAR(255) NOT NULL,
+			role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW()
+		)`);
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS issues(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(150) NOT NULL,
+            description TEXT NOT NULL,
+            type VARCHAR(20) NOT NULL,
+            status VARCHAR(20) NOT NULL DEFAULT 'open',
+            reporter_id INTEGER NOT NULL,
+			
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW()
+		)`);
+
+        console.log("DB connected");
+    } catch (error) {
+        console.log(error);
+    }
 };
